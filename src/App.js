b5e9b48cea3b970a7ba1e919ae37095cea85b7ef
rscottlundgren <rscottlundgren@gmail.com>
2020-07-28 16:38:51 -0400

    Add Typed.js, Create 1st Component - 1) Added Typed.js to package.json. 2) Updated App.js with Form & App component.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import Typed from 'react-typed'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Form extends React.Component {
+  state = {
+    input: ''
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.state.input)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} style={{
+        display: this.props.isComplete === false ? 'none' : ''
+      }}>
+        > <input
+          type="text"
+          autofocus="autofocus"
+          value={this.state.input}
+          onChange={event => this.setState({ input: event.target.value })}
+        />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    isComplete: false
+  }
+
+  render() {
+    return (
+      <div className="">
+        <Typed
+          strings={[
+            'Shall we play a game?']}
+          typeSpeed={40}
+          onComplete={self => {
+            self.cursor.remove()
+            this.setState({ isComplete: true })
+          }}
+        />
+        <p />
+        <Form isComplete={this.state.isComplete}/>
+      </div>
+    )
+  }
 }
 
 export default App;
