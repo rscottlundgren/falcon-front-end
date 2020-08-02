2a26af631e490d4048ad3784396f23c160d3d59b
rscottlundgren <rscottlundgren@gmail.com>
2020-08-02 11:06:59 -0400

    Create & Initialize 'Alerts' Folder & 'Messages' - 1. Create 'Alerts' folder. 2. Create 'Messages' file in 'Alerts' folder. 3. Initialize 'Messages' with two tester messages for initial user interaction.

---
+++ src/components/Alerts/Messages.js
@@ -0,0 +1,5 @@
+export default {
+	intro: 'Shall we play a game?',
+	gamesList:
+		"Falcon's Maze<br>Black Jack<br>Gin Rummy<br>Hearts<br>Bridge<br>Checkers<br>Chess<br>Poker<br>Fighter Combat<br>Guerilla Engagement<br>Desert Warfare<br>Air-to-Ground Actions<br>Theaterwide Tactical Warfare<br>Theaterwide Biotoxic &amp; Chemical Warfare<br><br>Global Thermonuclear War<br><br><br>Tic-Tac-Toe",
+}
