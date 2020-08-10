224e7b4d5a6124dc167548b5e27bd3fc09bf2685
rscottlundgren <rscottlundgren@gmail.com>
2020-08-10 12:48:16 -0400

    Create & Initialize "InputOutput" Component - 1. Created "InputOutput" component. 2. Initialized component with code from Terminal (better replication)

---
+++ src/components/InputOutput.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import Form from './Form'
+import Falcon from './Falcon'
+import Messages from './Alerts/Messages'
+
+class InputOutput extends Component {
+	state = {
+		output: Messages.intro,
+		userInput: '',
+		isComplete: false,
+		isDisabled: '',
+	}
+
+	markComplete = () => {
+		this.setState({
+			isComplete: true,
+		})
+	}
+
+	onSubmit = (event, userInput) => {
+		event.preventDefault()
+		console.log('OnSubmit located in "InputOutput" component')
+		this.setState({
+			userInput: userInput,
+			isDisabled: 'disabled',
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<Falcon
+					output={this.state.output}
+					markComplete={this.markComplete}
+					isComplete={this.state.isComplete}
+				/>
+				<p />
+				<Form
+					input={this.state.userInput}
+					onSubmit={this.onSubmit}
+					isComplete={this.state.isComplete}
+					isDisabled={this.state.isDisabled}
+				/>
+				<p />
+			</div>
+		)
+	}
+}
+
+export default InputOutput
