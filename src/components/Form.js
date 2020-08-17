33c11d0b2d730c925c15f1aef85ee057979aa967
rscottlundgren <rscottlundgren@gmail.com>
2020-08-17 13:39:25 -0400

    Move "Form.js" Event Handlers - 1. Migrated all event handlers to "Terminal.js". 2. Updated all prop-dependent key/values with access to "Terminal.js".

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,33 +1,28 @@
 import React from 'react'
 
 class Form extends React.Component {
-	state = {
-		input: this.props.input,
-	}
-
 	render() {
 		return (
 			<form
 				style={{
 					display: this.props.isComplete === false ? 'none' : '',
 				}}
-				onSubmit={(event) => {
-					this.props.onSubmit(event, this.state.input)
-				}}
+				onSubmit={this.props.handleUserSubmit}
 			>
 				{'> '}
 				<input
 					ref={(input) => input && input.focus()}
 					type='text'
-					disabled={this.props.isDisabled}
+					name='userInput'
+					disabled={this.props.form}
 					style={{
 						border: 'none',
 						outline: 'none',
 						backgroundColor: '#FFF',
 						color: '#000',
 					}}
-					value={this.state.input}
-					onChange={(event) => this.setState({ input: event.target.value })}
+					value={this.props.userInput}
+					onChange={this.props.handleUserInput}
 				/>
 			</form>
 		)
