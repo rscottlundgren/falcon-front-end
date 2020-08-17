91959210928174bbfc83b6f3609598d696dd6f51
rscottlundgren <rscottlundgren@gmail.com>
2020-08-17 13:41:09 -0400

    Update "Terminal.js" - 1. Moved all child component's state to "Terminal.js". 2. Moved all event handlers into "Terminal.js".

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,32 +1,89 @@
 import React, { Component } from 'react'
 import InputOutput from './InputOutput'
 import Messages from './Alerts/Messages'
+import _ from 'lodash'
 
 class Terminal extends Component {
 	state = {
 		conversation: [
 			{
+				form: '',
+				isComplete: false,
 				output: Messages.intro,
 				userInput: '',
-				isDisabled: '',
 			},
 		],
 	}
 
-	// onSubmit = (event) => {
+	markComplete = () => {
+		const interactionIndex = this.state.conversation.length - 1
+		let newConversation = [...this.state.conversation]
+		newConversation[interactionIndex].isComplete = true
+		this.setState({
+			conversation: newConversation,
+		})
+	}
+
+	handleUserInput = (event) => {
+		const interactionIndex = this.state.conversation.length - 1
+		let newConversation = [...this.state.conversation]
+		newConversation[interactionIndex].userInput = event.target.value
+		this.setState({
+			conversation: newConversation,
+		})
+	}
+
+	handleUserSubmit = (event, userInput) => {
+		event.preventDefault()
+		const interactionIndex = this.state.conversation.length - 1
+		let newConversation = [...this.state.conversation]
+		newConversation[interactionIndex].form = 'disabled'
+		this.setState({
+			conversation: [
+				newConversation,
+				{
+					form: '',
+					isComplete: false,
+					output: Messages.gamesList,
+					userInput: '',
+				},
+			],
+		})
+	}
+
+	// Logic for state change
+
+	// onSubmit = (event, data) => {
 	// 	event.preventDefault()
-	// 	console.log('OnSubmit located in "Terminal" component')
-	// 	// this.setState={
-	// 	// 	this.state.conversation.map(dialog => dialog.id === id ? {...dialog, userInput} : dialog)
-	// 	// }
+	// 	console.log({ event, data })
+	// 	console.log('Do you See me?', this.state.conversation.length)
+	// 	const lastConversation = _.last(this.state.conversation)
+	// 	lastConversation.isDisabled = data.isDisabled
+	// 	lastConversation.userInput = data.userInput
+	// 	this.setState({
+	// 		conversation: [
+	// 			...this.state.conversation,
+	// 			{
+	// 				output: Messages.gamesList,
+	// 				userInput: '',
+	// 				isDisabled: '',
+	// 			},
+	// 		],
+	// 	})
 	// }
 
 	render() {
+		const lastConversation = _.last(this.state.conversation)
 		return (
 			<div>
 				<InputOutput
-					output={this.state.conversation[0].output}
-					// onSubmit={this.onSubmit}
+					form={lastConversation.form}
+					isComplete={lastConversation.isComplete}
+					output={lastConversation.output}
+					userInput={lastConversation.userInput}
+					handleUserSubmit={this.handleUserSubmit}
+					handleUserInput={this.handleUserInput}
+					markComplete={this.markComplete}
 				/>
 			</div>
 		)
